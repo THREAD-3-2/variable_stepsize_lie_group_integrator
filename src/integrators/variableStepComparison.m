21364f3da11cab1f8c1534b1ac7f537d31b9cc92
Ergys Çokaj <ergys.cokaj@ntnu.no>
2022-08-18 17:23:21 +0200

    Delete unnecessary stuff

---
+++ src/integrators/variableStepComparison.m
@@ -0,0 +1,39 @@
+function [err,TT,Y] = variableStepComparison(vecField,action,z0,T,tol) 
+
+    chunk=500;
+    Z=zeros(length(z0),chunk);
+    TT=zeros(1,chunk);
+    Y=Z;
+    Y(:,1)=z0;
+
+    a = 1/4;
+    theta = 0.85;
+    i = 1;
+    h = T/499;
+    rejected = 0;
+
+    while TT(i)<T-5*eps
+        err = tol + 1;
+        while err>tol    
+%             [z,err] = VariableStepFreeRK4SE3N(f,action,h,Y(:,i));
+            [z,err] = variableRKMK45(vecField,action,Y(:,i),h);
+            accepted = (err<tol);
+            if accepted
+                i = i + 1;
+                Y(:,i) = z;
+                TT(i) = TT(i-1) + h;
+            else
+                rejected = rejected + 1;
+            end
+            h = min(theta * (tol/err)^a * h,T-TT(i));
+ 
+        end
+        if mod(i,chunk)==0
+            Y=[Y, Z];
+            TT=[TT zeros(1,chunk)];
+        end
+    end
+    Y=Y(:,1:i);
+    TT=TT(:,1:i); 
+    
+end
