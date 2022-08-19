0ff73392926496e380917aab0f62fb3b7531bafa
Ergys Çokaj <88881376+ergyscokaj@users.noreply.github.com>
2022-08-19 16:54:33 +0200

    Update actionSE3.m

--- src/actionSE3.m
+++ src/actionSE3.m
@@ -1,6 +1,13 @@
-function res = actionSE3(B,input)
+function res = actionSE3(B, input)
 
-    res = [B(1:3,1:3)*input(1:3); B(1:3,1:3)*input(4:6) + cross(B(:,end),(B(1:3,1:3)*input(1:3)))];
+% action on the SE3 group
+%
+% :param B: element of the Lie group SE3
+% :param input: element of the Lie algebra se3
+%
+% :returns: element of the Lie algebra se3
+
+res = [B(1:3,1:3)*input(1:3); B(1:3,1:3)*input(4:6) + cross(B(:,end),(B(1:3,1:3)*input(1:3)))];
 
 
 end
