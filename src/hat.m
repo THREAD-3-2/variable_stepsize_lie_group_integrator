150887cdb1b4a0002dd6dd8fedd931614b44c08b
Ergys Çokaj <88881376+ergyscokaj@users.noreply.github.com>
2022-08-19 17:59:12 +0200

    Update hat.m

--- src/hat.m
+++ src/hat.m
@@ -1,11 +1,14 @@
 function A = hat(v)
-
-    %It returns the skew symmetric matrix A associated to the vector v
+% hat map
+%
+% :param v: vector of 3 elements
+%
+% :returns: the skew symmetric matrix A associated to the vector v
 
     A = zeros(3);
-    A(1,2) = -v(3);
-    A(1,3) = v(2);
-    A(2,3) = -v(1);
+    A(1, 2) = - v(3);
+    A(1, 3) = v(2);
+    A(2, 3) = - v(1);
     
     A = A - A';
     
