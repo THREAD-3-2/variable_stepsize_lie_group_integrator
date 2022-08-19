12a5e24fa91aa7716b4843f03ef20931b36134a3
Ergys Çokaj <88881376+ergyscokaj@users.noreply.github.com>
2022-08-19 18:04:26 +0200

    Update assembleM.m

--- src/assembleM.m
+++ src/assembleM.m
@@ -1,21 +1,25 @@
-function M = assembleM(q,L,m)
+function M = assembleM(q, L, m)
+% assembleM
+%
+% :param q:
+% :param L:
+% :param m:
+%
+% :returns: 
 
-    N = floor(length(q)/3); %Number of connected pendulums
+    N = floor(length(q) / 3); % Number of connected pendulums
     
-    M = zeros(3*N,3*N);
+    M = zeros(3 * N, 3 * N);
 
-    for i = 1:N
-        for j = i+1:N
-            M(3*i-2:3*i,3*j-2:3*j) = sum(m(j:end)) * L(i) * L(j) * eye(3);
+    for i = 1 : N
+        for j = i + 1 : N
+            M(3 * i - 2 : 3 * i, 3 * j - 2 : 3 * j) = sum(m(j : end)) * L(i) * L(j) * eye(3);
         end
     end
     
     M = M + M';
     
-    for i = 1:N
-        M(3*i-2:3*i, 3*i-2:3*i) = sum(m(i:end)) * L(i)^2 * eye(3);
+    for i = 1 : N
+        M(3 * i - 2 : 3 * i, 3 * i - 2 : 3 * i) = sum(m(i : end)) * L(i) ^ 2 * eye(3);
     end
-    
-        
-
 end
