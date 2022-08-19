5502fc1663386599e10be3d52a1defade6c92e1e
Ergys Çokaj <88881376+ergyscokaj@users.noreply.github.com>
2022-08-19 18:06:32 +0200

    Update assembleR.m

--- src/assembleR.m
+++ src/assembleR.m
@@ -1,23 +1,28 @@
-function R = assembleR(q,L,m)
+function R = assembleR(q, L, m)
+% assembleR
+%
+% :param q:
+% :param L:
+% :param m:
+%
+% :returns: 
 
-    N = floor(length(q)/3); %Number of connected pendulums
+
+    N = floor(length(q) / 3); % Number of connected pendulums
     
-    R = zeros(3*N,3*N);
+    R = zeros(3 * N, 3 * N);
 
-    %Create upper triangular part
-    for i = 1:N
-        for j = i+1:N
-            R(3*i-2:3*i,3*j-2:3*j) = sum(m(j:end)) * L(i) * L(j) * hat(q(3*i-2:3*i))'*hat(q(3*j-2:3*j));
+    % Create upper triangular part
+    for i = 1 : N
+        for j = i + 1 : N
+            R(3 * i - 2 : 3 * i, 3 * j - 2 : 3 * j) = sum(m(j : end)) * L(i) * L(j) * hat(q(3 * i - 2 : 3 * i))' * hat(q(3 * j - 2 : 3 * j));
         end
     end
     
-    R = R + R'; %since it is symmetric
+    R = R + R'; % since it is symmetric
     
-    %Create the diagonal
-    for i = 1:N
-        R(3*i-2:3*i , 3*i-2:3*i) = sum(m(i:end)) * L(i)^2 * eye(3);
+    % Create the diagonal
+    for i = 1 : N
+        R(3 * i - 2 : 3 * i, 3 * i - 2 : 3 * i) = sum(m(i : end)) * L(i) ^ 2 * eye(3);
     end
-    
-        
-
 end
