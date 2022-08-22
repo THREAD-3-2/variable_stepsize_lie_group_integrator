03676f129d33a29479c9d69be8d718b74ae99e4e
Ergys Çokaj <88881376+ergyscokaj@users.noreply.github.com>
2022-08-22 09:15:45 +0200

    Update expSE3.m

--- src/expSE3.m
+++ src/expSE3.m
@@ -1,34 +1,29 @@
 function A = expSE3(input)
-
-% exponential map on the SE3 group
+% Exponential map on SE(3)
 %
-% :param input: 
+% :param input: element of the Lie algebra se(3) represented as [hat(u),v],
+%               with u and v 3x1 column vectors
 %
-% :returns: 
-
-    %we take as an input the element of SE(3) representable by [hat(u),v]
-    %so u and v are column 3-vectors
+% :returns: element of the group SE(3)
     
     u = input(1:3);
     v = input(4:6);
     theta = norm(u,2);
     
-    tol = 1e-16;  
+    tol = 1e - 16;  
     
-    if theta>tol
-        A = sin(theta)/theta;
-        B = (1-cos(theta))/(theta^2);
-        C = (1-A)/(theta^2);
-        V = eye(3) + B*hat(u) + C * hat(u) * hat(u);
-        A = [expRodrigues(u), V*v];
-    elseif theta==0
+    if theta > tol
+        A = sin(theta) / theta;
+        B = (1 - cos(theta)) / (theta^2);
+        C = (1 - A)/(theta ^ 2);
+        V = eye(3) + B * hat(u) + C * hat(u) * hat(u);
+        A = [expRodrigues(u), V * v];
+    elseif theta == 0
         A = [expRodrigues(u), v];
     else
-        Blow = 0.5-theta^2/24 + theta^4/720 - theta^6/40320;
-        Clow = (1/6-theta^2/120+theta^4/5040-theta^6/362880);
-        Vlow = eye(3) + Blow*hat(u) + Clow * hat(u) * hat(u);
-        A = [expRodrigues(u), Vlow*v];
-    
+        Blow = 0.5 - theta^2/24 + theta^4/720 - theta^6/40320;
+        Clow = (1/6 - theta^2/120 + theta^4/5040 - theta^6/362880);
+        Vlow = eye(3) + Blow * hat(u) + Clow * hat(u) * hat(u);
+        A = [expRodrigues(u), Vlow * v];
     end
-    
 end
