0e3ae64af97b843367cd00b4d552a3f1a4763ad4
Ergys Çokaj <88881376+ergyscokaj@users.noreply.github.com>
2022-08-22 09:23:54 +0200

    Update hat.m

--- src/hat.m
+++ src/hat.m
@@ -1,10 +1,9 @@
 function A = hat(v)
-% hat map
+% It returns the skew symmetric matrix A associated to the vector v
 %
-% :param v: vector of 3 elements
+% :param v: vector with 3 components
 %
-% :returns: the skew symmetric matrix A associated to the vector v
-
+% :returns: element of the Lie algebra so(3) (skew symmetric 3x3 matrix)
     A = zeros(3);
     A(1, 2) = - v(3);
     A(1, 3) = v(2);
