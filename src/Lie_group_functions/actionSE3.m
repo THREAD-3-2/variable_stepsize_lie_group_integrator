b5db35368005bc45eee11d3c003088e610e16cb1
Ergys Çokaj <88881376+ergyscokaj@users.noreply.github.com>
2022-08-22 10:44:30 +0200

    Update actionSE3.m

--- src/Lie_group_functions/actionSE3.m
+++ src/Lie_group_functions/actionSE3.m
@@ -1,13 +1,11 @@
 function res = actionSE3(B, input)
-
-% action on the SE3 group
+% action on the SE(3) group
 %
-% :param B: element of the Lie group SE3
-% :param input: element of the Lie algebra se3
+% :param B: element of the Lie group SE(3)
+% :param input: element of the Lie algebra se(3)
 %
-% :returns: element of the Lie algebra se3
+% :returns: element of the Lie algebra se(3)
 
-res = [B(1:3,1:3)*input(1:3); B(1:3,1:3)*input(4:6) + cross(B(:,end),(B(1:3,1:3)*input(1:3)))];
-
+res = [B(1:3, 1:3) * input(1:3); B(1:3, 1:3) * input(4:6) + cross(B(:, end), (B(1:3, 1:3) * input(1:3)))];
 
 end
